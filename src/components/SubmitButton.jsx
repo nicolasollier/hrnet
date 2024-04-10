9c734ce6ce129b15e97b47fd5168788ad569aeca
Nicolas Ollier <91204591+nicolasollier@users.noreply.github.com>
2024-04-10 11:31:36 +0200

    add new styles to app

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,26 @@
 import PropTypes from 'prop-types';
 
 const SubmitButton = ({ type, onClick }) => {
+  const buttonStyles = {
+    backgroundColor: '#E9E9EC',
+    padding: '1rem 1.5rem',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    width: '100%',
+    marginTop: '1rem',
+  }
+
   return (
-    <button type={type} onClick={onClick}>Save</button>
+    <button
+      onMouseEnter={(e) => e.target.style.backgroundColor = '#D0D0D5'}
+      onMouseLeave={(e) => e.target.style.backgroundColor = '#E9E9EC'}
+      style={buttonStyles}
+      type={type}
+      onClick={onClick}
+    >
+      Save
+    </button>
   );
 }
 
