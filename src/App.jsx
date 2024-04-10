68008d22afc6fa545abb1f8f8c92b0d0e3de9e35
Nicolas Ollier <91204591+nicolasollier@users.noreply.github.com>
2024-04-10 17:04:37 +0200

    update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import Form from './components/Form'
 import { Link } from 'react-router-dom'
+import Modal from 'simplest-react-modalbox'
 
 function App() {
   return (
     <>
+      <Modal title={"Hello"} text={"world!"} status={'refused'} />
       <Header>
         <h1>HRnet</h1>
         <Link to='/employees'>View current employees</Link>
